a9a353883280559a484c9785cef08bd1278d86a6
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-03-01 02:53:40 -0400

    Dejar main vacio para futuras pruebas

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-        ReservaService myRs = new ReservaService();
-        myRs.crearReserva("Israel", LocalDate.now(), LocalTime.now());
-        myRs.crearReserva("Luisa", LocalDate.now(), LocalTime.now());
-        myRs.crearReserva("Maria", LocalDate.now(), LocalTime.now());
 
-        myRs.listarReservas();
     }
 }
