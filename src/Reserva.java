6e9ab7201cf472f62c52d2ea3f49a0bc8bd19695
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-03-01 03:00:50 -0400

    Agregar robustez a la clase Reserva

--- src/Reserva.java
+++ src/Reserva.java
@@ -9,15 +9,48 @@ public class Reserva {
     private static int idCount = 1;
 
     public Reserva(String nombre, LocalDate fecha, LocalTime hora) {
-        id = idCount;
+        this.id = idCount;
         idCount++;
         this.nombre = nombre;
         this.fecha = fecha;
         this.hora = hora;
     }
 
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.isBlank()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacío.");
+        }
+        this.nombre = nombre;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        if (fecha == null) {
+            throw new IllegalArgumentException("La fecha no puede ser null.");
+        }
+        this.fecha = fecha;
+    }
+
+    public void setHora(LocalTime hora) {
+        if (hora == null) {
+            throw new IllegalArgumentException("La hora no puede ser null.");
+        }
+        this.hora = hora;
+    }
+
     public int getId() {
         return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
     }
 
     @Override
