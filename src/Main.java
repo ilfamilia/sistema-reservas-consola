85fc5cf3228801db29eb517f0c45cbc89ec46172
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-03-02 02:16:40 -0400

    Agregar switch para el menú más métodos de ayuda para validación y entrada de datos

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Scanner;
@@ -7,11 +8,42 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
+        ReservaService service = new ReservaService();
+
         int opcion;
 
         do {
             mostrarMenu();
             opcion = leerEntero(sc, "Seleccione una opción: ");
+
+            switch (opcion) {
+                case 1: {
+                    System.out.println("\n--- Crear Reserva ---");
+
+                    String nombre = leerTexto(sc, "Nombre: ");
+                    LocalDate fecha = leerFecha(sc, "Año: ", "Mes: ", "Día: ");
+                    LocalTime hora = leerHora(sc, "Hora: ", "Minutos: ");
+
+                    Reserva r = new Reserva(nombre, fecha, hora);
+
+                    boolean operacionValida = service.crearReserva(r);
+
+                    if (operacionValida) {
+                        System.out.println("\nReserva creada correctamente.");
+                    } else {
+                        System.out.println("\nNo se pudo crear la reserva.");
+                    }
+
+                    break;
+                }
+
+                case 6:
+                    System.out.println("\nSaliendo...");
+                    break;
+
+                default:
+                    System.out.println("\nOpción inválida. Intenta de nuevo.");
+            }
 
         } while (opcion != 6);
     }
@@ -40,4 +72,67 @@ public class Main {
             }
         }
     }
+
+
+    private static String leerTexto(Scanner sc, String mensaje) {
+        while (true) {
+            System.out.print(mensaje);
+            String line = sc.nextLine();
+            if (!line.trim().isEmpty()) return line.trim();
+            System.out.println("\nEste campo no puede estar vacío.\n");
+        }
+    }
+
+    private static LocalDate leerFecha(Scanner sc, String mensaje1, String mensaje2, String mensaje3) {
+        while (true) {
+
+            System.out.print(mensaje1);
+            String line1 = sc.nextLine();
+
+            System.out.print(mensaje2);
+            String line2 = sc.nextLine();
+
+            System.out.print(mensaje3);
+            String line3 = sc.nextLine();
+
+            try {
+
+                int anio = Integer.parseInt(line1.trim());
+                int mes = Integer.parseInt(line2.trim());
+                int dia = Integer.parseInt(line3.trim());
+
+                LocalDate fecha = LocalDate.of(anio, mes, dia);
+
+                return fecha;
+
+            } catch (DateTimeException e) {
+                System.out.println("\nFecha inválida. Intente de nuevo.");
+            }
+        }
+    }
+
+    private static LocalTime leerHora(Scanner sc, String mensaje1, String mensaje2) {
+        while (true) {
+
+            System.out.print(mensaje1);
+            String line1 = sc.nextLine();
+
+            System.out.print(mensaje2);
+            String line2 = sc.nextLine();
+
+            try {
+
+                int horas = Integer.parseInt(line1.trim());
+                int minutos = Integer.parseInt(line2.trim());
+
+                LocalTime hora = LocalTime.of(horas, minutos);
+
+                return hora;
+
+            } catch (DateTimeException e) {
+                System.out.println("\nHora inválida. Intente de nuevo.");
+            }
+        }
+    }
+
 }
